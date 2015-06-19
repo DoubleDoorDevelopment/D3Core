dc7a04aa703df46c81e2e6c8f210e7b3db9d9aa8
Dries007 <admin@dries007.net>
2015-06-20 01:39:10 +0200

    feedback on /setLoginMessage

--- src/main/java/net/doubledoordev/d3core/util/CommandSetLoginMessage.java
+++ src/main/java/net/doubledoordev/d3core/util/CommandSetLoginMessage.java
@@ -1,9 +1,12 @@
 package net.doubledoordev.d3core.util;
 
+import com.google.gson.JsonParseException;
 import net.doubledoordev.d3core.D3Core;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.IChatComponent;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -33,18 +36,28 @@ public class CommandSetLoginMessage extends CommandBase
         {
             File file = new File(D3Core.getFolder(), "loginmessage.txt");
             if (file.exists()) file.delete();
+            sender.addChatMessage(new ChatComponentTranslation("d3.core.cmd.setloginmessage.removed"));
         }
         else
         {
+            String txt = func_82360_a(sender, args, 0);
             try
             {
-                FileUtils.writeStringToFile(new File(D3Core.getFolder(), "loginmessage.txt"), func_82360_a(sender, args, 0));
+                FileUtils.writeStringToFile(new File(D3Core.getFolder(), "loginmessage.txt"), txt);
             }
             catch (IOException e)
             {
                 throw new RuntimeException(e);
             }
+            sender.addChatMessage(new ChatComponentTranslation("d3.core.cmd.setloginmessage.success"));
+            try
+            {
+                sender.addChatMessage(IChatComponent.Serializer.func_150699_a(txt));
+            }
+            catch (JsonParseException jsonparseexception)
+            {
+                sender.addChatMessage(new ChatComponentText(txt));
+            }
         }
-        sender.addChatMessage(new ChatComponentTranslation("d3.core.cmd.setloginmessage.success"));
     }
 }
