1a8dbb9ef4809a4cafd5043245719b3b3017f2e1
Andrew W <marwat208@gmail.com>
2016-07-03 04:13:43 -0400

    Updated with Burn Time changes
    updated to include Burn Time Changes
    updated version

--- src/main/java/net/doubledoordev/d3core/util/ForgeEventHandler.java
+++ src/main/java/net/doubledoordev/d3core/util/ForgeEventHandler.java
@@ -35,19 +35,15 @@ package net.doubledoordev.d3core.util;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameData;
 
-import net.minecraft.command.CommandBase;
-import net.minecraft.command.CommandStats;
-import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.command.ServerCommandManager;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentString;
@@ -72,6 +68,7 @@ public class ForgeEventHandler
     public boolean enableStringID;
     public boolean enableUnlocalizedName;
     public boolean enableOreDictionary;
+    public boolean enableBurnTime;
     public boolean nosleep;
     public boolean printDeathCoords = true;
     public boolean claysTortureMode;
@@ -86,6 +83,7 @@ public class ForgeEventHandler
             if (enableStringID) event.getToolTip().add(TextFormatting.DARK_AQUA + event.getItemStack().getItem().getRegistryName().toString());
             if (enableUnlocalizedName) event.getToolTip().add(TextFormatting.DARK_GREEN + event.getItemStack().getUnlocalizedName());
             if (enableOreDictionary) for (int id : OreDictionary.getOreIDs(event.getItemStack())) event.getToolTip().add(TextFormatting.DARK_PURPLE + OreDictionary.getOreName(id));
+            if (enableBurnTime && TileEntityFurnace.isItemFuel(event.getItemStack())) event.getToolTip().add(TextFormatting.GOLD + "Burns for " + TileEntityFurnace.getItemBurnTime(event.getItemStack()) + " ticks");
         }
     }
 
